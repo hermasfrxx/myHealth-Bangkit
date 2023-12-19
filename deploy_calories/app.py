0bc0af79a9cd689c411c4b3a0b62139658632917
bahrur <bahrur1256@gmail.com>
2023-12-20 00:36:33 +0700

    fix deploy model using fast

--- deploy_calories/app.py
+++ deploy_calories/app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import MySQLdb
 from keras.models import load_model
 import numpy as np
+
+db_config ={
+    'host':'localhost',
+    'user': 'root',
+    'passwd':'',
+    'db':'food_data'
+}
+conn = MySQLdb.connect(**db_config)
 
 app = FastAPI()
 
@@ -11,10 +20,18 @@ class ModelInput(BaseModel):
     Height: float
     Weight: float
 
+# class Calori(BaseModel):
+#     Food: str
+#     Grams: int
+#     Calories: int
+#     Protein:int
+#     Fat: int
+#     Id_Calories= int
+
 # Load the model directly
 calories = load_model('model.h5')
 
-@app.post('/calories_prediction')
+@app.get('/calories_prediction')
 def calories_pred(input_parameters: ModelInput):
     sex = input_parameters.Sex
     age = input_parameters.Age
@@ -24,15 +41,21 @@ def calories_pred(input_parameters: ModelInput):
     # Ensure input has the correct shape
     input_data = np.array([[sex, age, hei, wei]])
 
-    prediction = calories.predict(input_data)
+    # Get the raw predictions (probabilities for each class)
+    raw_predictions = calories.predict(input_data)
 
-    predicted_class = np.argmax(prediction, axis=1)
-
-    if predicted_class[0] == 0:
-        return 'low calories'
-    elif predicted_class[0] == 1:
-        return 'Medium calories'
-    elif predicted_class[0] == 2:
-        return 'high calories'
+    # Get the index of the class with the highest probability
+    predicted_class = np.argmax(raw_predictions, axis=1)[0]
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM foodi WHERE Id_Calories=%s",(int(predicted_class),))
+        result = cursor.fetchall()
+    if result:
+        # Convert the result to a list of dictionaries for better JSON serialization
+        food_data_list = [
+            {'Id': row[0],'Food': row[1],'Grams':row[2],'Calories':row[3],'Protein':row[4],'Fat':row[5]}  # Replace column1, column2, etc. with actual column names
+            for row in result
+        ]
+        return food_data_list
     else:
-        return 'out of context'
+        return {'message': 'No food data found.'}
+
--- deploy_calories/req.py
+++ deploy_calories/req.py
@@ -4,14 +4,14 @@ import requests
 url = 'http://127.0.0.1:8000/calories_prediction'
 
 input_data_for_model ={
-    'Sex': 1,
-    'Age': 20,
-    'Height':170.0, 
-    'Weight': 75.0
+    'Sex': 0,  # Assuming 0 represents male (adjust based on your encoding)
+    'Age': 12,
+    'Height': 0,
+    'Weight': 0
 }
 
 input_json = json.dumps(input_data_for_model)
 
-response = requests.post(url, data=input_json)
+response = requests.get(url, data=input_json)
 
 print(response.text)
