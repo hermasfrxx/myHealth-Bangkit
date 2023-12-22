31307b02f3872c0ca1c27546c391c5f63bc4b9ea
syaifa21 <alisyaifarudin04@gmail.com>
2023-12-22 11:04:25 +0700

    aa

--- app/src/main/java/com/dicoding/myhealth/api/ApiService.kt
+++ app/src/main/java/com/dicoding/myhealth/api/ApiService.kt
@@ -1,27 +1,41 @@
-package com.dicoding.myhealth.api
+package com.dicoding.myhealth.API
 
 
-import com.dicoding.myhealth.api.response.LoginResponse
-import com.dicoding.myhealth.api.response.RegisterResponse
+
+import com.dicoding.myhealth.Response.GetAllStoriesResponse
+import com.dicoding.myhealth.Response.LoginResponse
+import com.dicoding.myhealth.Response.RegisterResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
-    @FormUrlEncoded
-    @POST("signin")
-    fun signin(
-        @Field("username") name: String,
-        @Field("password") password: String
-    ): Call<LoginResponse>
 
-    @FormUrlEncoded
-    @POST("signup")
-    fun signup(
-        @Field("username") name: String,
-        @Field("email") email: String,
-        @Field("password") password: String,
-
+        @FormUrlEncoded
+        @POST("register")
+        fun register(
+            @Field("name") name: String,
+            @Field("email") email: String,
+            @Field("password") password: String
         ): Call<RegisterResponse>
+
+        @FormUrlEncoded
+        @POST("login")
+        fun login(
+            @Field("email") email: String,
+            @Field("password") password: String
+        ): Call<LoginResponse>
+    @GET("stories")
+    fun getAllStories(
+    ): Call<GetAllStoriesResponse>
+
 }
